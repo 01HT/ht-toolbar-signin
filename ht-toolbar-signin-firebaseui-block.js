d0c3d27ef3358e5c12801c7ba7da29a7b3cc5652
Maksim Yurkov <yurkov.maksim.sergeevich@gmail.com>
2018-10-22 20:13:59 +0300

    Update ht-toolbar-signin-firebaseui-block.js
    remove twitter, cant approve app and get keys

--- ht-toolbar-signin-firebaseui-block.js
+++ ht-toolbar-signin-firebaseui-block.js
@@ -50,7 +50,7 @@ class HTToolbarSigninFirebaseuiBlock extends LitElement {
             signInOptions: [
                 firebase.auth.GoogleAuthProvider.PROVIDER_ID,
                 firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-                firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+                // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
                 firebase.auth.GithubAuthProvider.PROVIDER_ID,
                 {
                     provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
